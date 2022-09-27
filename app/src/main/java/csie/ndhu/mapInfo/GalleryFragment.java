13d3c1270690dab9973b5ab94af8751974d7bfc5
Francis Yang <yangfrancis0331@gmail.com>
2022-09-27 16:47:11 +0800

    nav init

---
+++ app/src/main/java/csie/ndhu/mapInfo/GalleryFragment.java
@@ -0,0 +1,34 @@
+package csie.ndhu.mapInfo;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GalleryFragment extends Fragment {
+
+    private GallerySharedViewModel mViewModel;
+
+    public static GalleryFragment newInstance() {
+        return new GalleryFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.gallery_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(GallerySharedViewModel.class);
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/csie/ndhu/mapInfo/GallerySharedViewModel.java
@@ -0,0 +1,7 @@
+package csie.ndhu.mapInfo;
+
+import androidx.lifecycle.ViewModel;
+
+public class GallerySharedViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
