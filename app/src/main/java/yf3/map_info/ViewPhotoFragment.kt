6bd8babfe4832811f4e2fd8f40a79c26dfe88631
Francis Yang <yangfrancis0331@gmail.com>
2022-10-09 23:50:01 +0800

    fixed overlapping

--- app/src/main/java/yf3/map_info/ViewPhotoFragment.kt
+++ app/src/main/java/yf3/map_info/ViewPhotoFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
 
 class ViewPhotoFragment: Fragment() {
 
