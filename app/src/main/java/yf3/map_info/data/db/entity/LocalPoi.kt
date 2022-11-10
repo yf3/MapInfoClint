dd4a1f1ea2d97b065e88fb826f91b77c2a0ae369
Francis Yang <yangfrancis0331@gmail.com>
2022-11-11 02:07:47 +0800

    added FK

--- app/src/main/java/yf3/map_info/data/db/entity/LocalPoi.kt
+++ app/src/main/java/yf3/map_info/data/db/entity/LocalPoi.kt
@@ -1,16 +1,33 @@
 package yf3.map_info.data.db.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(foreignKeys = [
+    ForeignKey(
+        entity = LocalPoiType::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("poi_type_id"),
+        onDelete = ForeignKey.SET_DEFAULT),
+    ForeignKey(
+        entity = LocalMap::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("map_id"),
+        onDelete = ForeignKey.SET_DEFAULT)
+    ]
+)
 data class LocalPoi (
     @PrimaryKey val id: Int,
     val title: String,
+    @ColumnInfo(name = "photo_path")
     val photoAbsPath: String,
     val longitude: Double,
     val latitude: Double,
     val description: String,
+    @ColumnInfo(name = "poi_type_id")
     val poiType: Int,
+    @ColumnInfo(name = "map_id")
     val mapID: Int
 )
--- app/src/main/java/yf3/map_info/data/db/entity/LocalPoiType.kt
+++ app/src/main/java/yf3/map_info/data/db/entity/LocalPoiType.kt
@@ -1,7 +1,20 @@
 package yf3.map_info.data.db.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+
+@Entity(foreignKeys = [
+    ForeignKey(
+        entity = LocalMap::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("map_id"),
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class LocalPoiType(
     val id: Int,
     val name: String,
+    @ColumnInfo(name = "map_id")
     val mapId: Int,
 )
